3c8d3e964b8ccf63781499ac5c728cde6c124697
Arifin Rahman <arifinrahman6@gmail.com>
2020-06-26 19:12:23 -0400

    Messages view
    Once a channel is selected, a user can see all messages and send them
    too. Stored in a queue object

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import os
+from queue import Queue
 
 from flask import Flask, session, redirect, render_template, request, url_for, session
 from flask_socketio import SocketIO, emit
@@ -9,19 +11,51 @@ app.secret_key = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 channel_list = []
+messages = {}
+
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
+    '''User can enter screen name, can add channels and shows list of channels'''
     if request.method == 'POST':
         if 'name' in request.form:
             session['name'] = request.form['name']
         if 'new_channel' in request.form:
-            channel_list.append(request.form['new_channel'])
+            # Create a new channel
+            new_channel = request.form['new_channel']
+            messages[new_channel] = Queue(maxsize=100)
+            channel_list.append(new_channel)
 
         return redirect(url_for('index', name=session['name'], channels=channel_list))
     
-    return render_template('index.html', name=session['name'], channels=channel_list)
+    if 'name' in session:
+        return render_template('index.html', name=session['name'], channels=channel_list)
+    
+    return render_template('index.html', channels=channel_list)
 
-@app.route("/channels/<channel_name>")
+
+@app.route("/channels/<channel_name>", methods=['POST'])
 def channel(channel_name):
-    return render_template('channel.html', channel_name='{channel_name}')
+    '''Shows messages in the channel'''
+    if request.method == 'POST':
+        # A formatted message includes message, time, and name
+        now = datetime.now()
+        formatted_now = now.strftime("%d/%m/%Y %H:%M")
+        new_message = (request.form['message'], formatted_now, session['name'])
+
+        # Pop a message if queue gets full
+        if messages[channel_name].full():
+            messages[channel_name].get()
+
+        messages[channel_name].put(new_message)
+
+        return redirect(url_for('channel', channel_name=channel_name, messages=messages[channel_name].queue))
+
+    return render_template('channel.html', channel_name=channel_name, messages=messages[channel_name].queue)
+
+
+# @app.route('/leave')
+# def leave():
+#     # remove the name from the session if it's there
+#     session.pop('name', None)
+#     return redirect(url_for('index'))

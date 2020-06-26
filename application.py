26ee6ff2a2f7c5269673a225c71150bd8fd4781f
Arifin Rahman <arifinrahman6@gmail.com>
2020-06-25 22:26:28 -0400

    Display name
    Using session, when a user visits, it will prompt for name and remember
    it even if the page is closed

--- application.py
+++ application.py
@@ -1,15 +1,22 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, session, redirect, render_template, request, url_for, session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.secret_key = os.getenv("SECRET_KEY")
+
 socketio = SocketIO(app)
 
-
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        session['name'] = request.form['name']
+        return redirect(url_for('index', name=session['name']))
+    
+    if 'name' in session:
+        return render_template('index.html', name=session['name'])
+    
     return render_template('index.html')
 
 @app.route("/channels")
